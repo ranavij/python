36af8985a81ca94bea8c7fc4b44d33058a562c8c
ranavij <ranavij84@gmail.com>
2018-09-06 13:22:03 +0530

    python

---
+++ assignment12.py
@@ -0,0 +1,16 @@
+#Q1
+
+import datetime
+
+x = datetime.datetime.now()
+print(x)
+
+#Q2
+
+import webbrowser
+webbrowser.open("https://www.youtube.com")
+
+#Q3
+import os
+os.rename('file.txt','file.jpg')
+© 2018 GitHub, Inc.
